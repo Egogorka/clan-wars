dc89a666ba32bd43d247b4776a3f6d1aa1cbe26b
Egogorka <bike034@mail.ru>
2018-11-05 16:49:01 +0700

    initial

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/navbar/_.css">
+</head>
+<body>
+
+<!-- Navbar </ -->
+
+<a class="nav a1" href="index.php">
+    MAIN
+</a>
+<a class="nav a2" href="#">
+    CLAN
+</a>
+<a class="nav a3" href="#">
+    SESSIONS
+</a>
+<a class="nav a4" href="#">
+    MAPS
+</a>
+
+<!-- Navbar />, Logo </ -->
+
+
+
+<!-- Logo />, Container </ -->
+
+
+</body>
+
+<script type="text/javascript" src="/navbar/_.js"></script>
+
+</html>
